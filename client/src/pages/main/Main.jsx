dec013d890206ca3bda05db6a340e36cc4cfccc9
wungthing <wungthingkashungnao123@gmail.com>
2023-12-23 10:59:49 +0530

    updated the filter method to delete skills

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -112,6 +112,7 @@ const Main = () => {
                                 ),
                               })
                             }
+                            onChange={findTheJOb}
                           >
                             x
                           </span>
